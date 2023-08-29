f08962ddd7a66f512711729b93d57e650165fdf9
Dave Vause <dvause@gmail.com>
2023-08-29 19:48:57 -0400

    Add test for CompanyNameMatchKey

---
+++ tests/api/CompanyNameMatchKey.test.ts
@@ -0,0 +1,57 @@
+import axios, { AxiosHeaders, AxiosResponse } from 'axios';
+import { getCompanyNameMatchKey } from '../../src/api/CompanyNameMatchKey';
+import { CompanyNameMatchKeyRequest } from '../../src/models/CompanyNameMatchKeyRequest';
+import { MatchKeyResponse } from '../../src/models/MatchKeyResponse';
+
+jest.mock('axios');
+
+describe('getCompanyNameMatchKey', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  it('returns a simKey when the request is valid', async () => {
+    // Arrange
+    const request: CompanyNameMatchKeyRequest = {
+      companyName: 'Apple, Inc',
+      apiKey: 'test-api-key',
+    };
+    const mockResponse: AxiosResponse<MatchKeyResponse> = {
+      data: { simKey: '12345', code: 'Success', credits: '9999' },
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {
+        headers: new AxiosHeaders(),
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+    // Act
+    const result = await getCompanyNameMatchKey(request);
+
+    // Assert
+    expect(result.simKey).toEqual('12345');
+  });
+
+  it('throws an error when the status is 403', async () => {
+    // Arrange
+    const request: CompanyNameMatchKeyRequest = {
+      companyName: 'Apple, Inc',
+      apiKey: 'test-api-key',
+    };
+    const mockResponse: AxiosResponse = {
+      data: {},
+      status: 403,
+      statusText: 'Bad Request',
+      headers: {},
+      config: {
+        headers: new AxiosHeaders(),
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+    // Act & Assert
+    await expect(getCompanyNameMatchKey(request)).rejects.toThrow(
+      'Invalid API key: You do not have permission to use this resource.',
+    );
+  });
+});
